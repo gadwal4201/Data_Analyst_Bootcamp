4df4bb2b58caed8e9ed7f3019b174db05e8491c1
gadwal4201 <gadwalpankaj1435@gmail.com>
2025-09-10 17:24:04 +0530

    Functions Revision

---
+++ Python/Modules/package/__init__.py
@@ -0,0 +1,3 @@
+'''
+__init__.py file is a special file used in pythonto define packages
+and initialize them. It can be an empty file or contain initialization code for the package.'''
---
+++ Python/Modules/test.py
@@ -0,0 +1,6 @@
+from package.maths import *
+from package.subppackages.mult import multiplication
+
+print(subtraction(10, 5))  # 5
+print(addition(10, 5))  # 15
+print(multiplication(10, 5))  # 50
